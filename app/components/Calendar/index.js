92b71c6af2dcedc0fa52bff0d6689b517dfb8292
Raidel <raidel.torres@domandtom.com>
2018-07-09 11:13:39 -0500

    adding the calendar generator

--- app/components/Calendar/index.js
+++ app/components/Calendar/index.js
@@ -9,6 +9,7 @@ import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import moment from 'moment';
 
 // Importing the defined actions
 import * as HomeActions from '../../containers/Home/actions';
@@ -20,10 +21,60 @@ const CalendarContainer = styled.div`
   .inputContainer {
     margin: 1rem;
   }
+
+  table {
+    width: 14rem;
+    border-collapse: separate;
+    border-spacing: 0.1rem;
+    font-size: 0.8rem;
+    margin-top: 2rem;
+
+    .month-year-cell {
+      background-color: #f6faeb;
+    }
+
+    td {
+      width: 2rem;
+      height: 2rem;
+      text-align: center;
+
+      &.active {
+        background-color: #c3db9f;
+      }
+
+      &.inactive {
+        background-color: #cccccc;
+      }
+    }
+  }
 `;
 
 // Calendar component
 class Calendar extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor() {
+    super();
+
+    this.state = {
+      calDaysLabels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+      calMonthsLabels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      calDaysInMonth: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+      calendar: true,
+    };
+  }
+
+  getNumWeeksInMonth(currentMonth, currentYear) {
+    const date = moment().set({
+      year: currentYear,
+      month: currentMonth - 1,
+    });
+
+    if (currentMonth === 12) {
+      return (date.weeksInYear() - date.startOf('month').weeks()) + 2;
+    }
+
+    return (date.endOf('month').weeks() - date.startOf('month').weeks()) + 1;
+  }
+
   changeDate(event) {
     // Is not really needed the use of redux since I will use only one component
     this.props.dispatch(HomeActions.changeDate(event.target.value));
@@ -39,8 +90,128 @@ class Calendar extends React.Component { // eslint-disable-line react/prefer-sta
     this.props.dispatch(HomeActions.changeCountry(event.target.value));
   }
 
+  generateCalendar() {
+    this.setState({
+      calendar: true,
+    });
+  }
+
   renderCalendar() {
-    return this.props.calendarData.country;
+    let pendingDays = this.props.calendarData.days; // Days to extend the calendar
+    let date = this.props.calendarData.date.split('-'); // Starting date
+    let currentYear = Number(date[0]); // Current year of the starting date
+    let currentMonth = Number(date[1]); // Current month of the starting date
+    let currentDay = Number(date[2]); // Current day of the starting date
+
+    // Starting date object and substracting one month since months in moment starts in 0
+    date = moment().set({
+      year: currentYear,
+      month: currentMonth - 1,
+      date: currentDay,
+    });
+
+    const dayOfWeek = date.weekday(); // Getting the day of the week
+    let weekOfMonth = (date.week() - moment(date).startOf('month').week()) + 1; // Getting the week number in the current month
+    let weeksInMonth = this.getNumWeeksInMonth(currentMonth, currentYear); // Getting the number of weeks in the current month
+    let startingDay = currentDay - dayOfWeek;
+
+    // ------------------------------------------
+    // Generating Calendar
+    // ------------------------------------------
+
+    // Creating the base objects to push the components of the table
+    const calendar = [];
+    let tempDayWeek = 0;
+
+    // Going trough all the months
+    while (pendingDays > 0) {
+      const weeks = [];
+
+      // Going trough all the weeks in the current month
+      while (weekOfMonth <= weeksInMonth && pendingDays > 0) {
+        const days = [];
+
+        // Going trough all the days in the current week
+        for (let i = 0; i < 7; i += 1) {
+          let day = null;
+
+          // Validating early end
+          if (pendingDays <= 0) {
+            startingDay = 32;
+          }
+
+          if (startingDay < currentDay) {
+            day = <td key={Math.random()} className="inactive" />;
+          } else if (startingDay > this.state.calDaysInMonth[currentMonth - 1]) {
+            day = <td key={Math.random()} className="inactive" />;
+
+            // Validating the end of month
+            tempDayWeek = i;
+            startingDay = -10;
+          } else {
+            day = <td key={Math.random()} className="active">{startingDay}</td>;
+
+            // We are only restarting a day if it was a valid day
+            pendingDays -= 1;
+
+            // Validating if the month ends with 0 inactive days
+            if (startingDay + tempDayWeek === weeksInMonth * 7) {
+              tempDayWeek = 0;
+              startingDay = -10;
+            }
+          }
+
+          startingDay += 1;
+          days.push(day);
+        }
+
+        const week = <tr key={Math.random()}>{days}</tr>;
+        weeks.push(week);
+        weekOfMonth += 1;
+      }
+
+      const month = (
+        <Grid
+          item
+          xs={12}
+          key={Math.random()}
+          container
+          alignItems={'center'}
+          justify={'center'}
+        >
+          <table>
+            <thead>
+              <tr>
+                {this.state.calDaysLabels.map((item) => <th key={Math.random()}>{ item }</th>)}
+              </tr>
+              <tr>
+                <th colSpan="7" className="month-year-cell">{this.state.calMonthsLabels[currentMonth - 1]} {currentYear}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {weeks}
+            </tbody>
+          </table>
+        </Grid>
+      );
+
+      calendar.push(month);
+
+      if (currentMonth < 12) {
+        currentMonth += 1;
+      } else {
+        currentMonth = 1;
+        currentYear += 1;
+      }
+
+      weekOfMonth = 1;
+      startingDay = 1 - tempDayWeek;
+      currentDay = 1;
+
+      weeksInMonth = this.getNumWeeksInMonth(currentMonth, currentYear);
+    }
+
+    return calendar;
   }
 
   render() {
@@ -81,13 +252,11 @@ class Calendar extends React.Component { // eslint-disable-line react/prefer-sta
                 onChange={(e) => { this.changeCountry(e); }}
               />
             </div>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" onClick={() => { this.generateCalendar(); }}>
               Generate Calendar
             </Button>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            { this.renderCalendar() }
-          </Grid>
+          { this.state.calendar ? this.renderCalendar() : null }
         </Grid>
       </CalendarContainer>
     );
